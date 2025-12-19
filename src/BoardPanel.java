aab4f218b85cb89814e6bba8825a621c02d3a14b
thaliaharnumfathina <thaliaharnum@gmail.com>
2025-12-19 12:41:39 +0700

    New Update: Final polish on graphics and core board logic

--- src/BoardPanel.java
+++ src/BoardPanel.java
@@ -16,11 +16,9 @@ public class BoardPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
 
-        // Enable anti-aliasing
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
-        // Translate to add padding
         g2.translate(10, 10);
 
         if (gameBoard != null) {
